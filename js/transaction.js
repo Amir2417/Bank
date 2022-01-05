38dae10442be1acf6cf813745a8cec3c06dd5361
Amir2417 <amirsohelto@gmail.com>
2022-01-05 23:33:06 +0600

    withdraw section calculation

--- js/transaction.js
+++ js/transaction.js
@@ -8,4 +8,39 @@ document.getElementById('depoBtn').addEventListener('click',function(){
     depoText.innerText = totalValue;
     inputValue.value ='';
 
+    //balance update
+    const balanceText = document.getElementById('balanceTotal');
+    const balanceValue = parseFloat(balanceText.innerText) ;
+    //calculation
+
+    const TotalBalance = newValue +balanceValue;
+
+    balanceText.innerText = TotalBalance;
+
+});
+
+// withdraw Section part
+
+document.getElementById('withdrawBtn').addEventListener('click',function(){
+    const withdrawAmount = document.getElementById('withdraw-amount');
+
+    const withdrawMoney = parseFloat(withdrawAmount.value);
+
+    // pass the value in withdraw section
+
+    const previousWithdraw = document.getElementById('withdrawTotal');
+
+    const withdrawBalance = parseFloat(previousWithdraw.innerText);
+    const totalWithdraw = withdrawMoney + withdrawBalance;
+
+    previousWithdraw.innerText = totalWithdraw;
+    withdrawAmount.value ='';
+
+    const balanceText = document.getElementById('balanceTotal');
+    const balanceValue = parseFloat(balanceText.innerText) ;
+    //calculation
+
+    const TotalBalance = balanceValue - withdrawMoney;
+
+    balanceText.innerText = TotalBalance;
 });
